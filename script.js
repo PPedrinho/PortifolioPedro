59f386eb236f515ecfb666429fa3c437fc6dbfb7
PPedrinho <peulima2004@gmail.com>
2025-04-01 19:07:49 -0300

    COMMIT

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll("nav a");
-
-    links.forEach(link => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            const target = document.querySelector(link.getAttribute("href"));
-            if (target) {
-                window.scrollTo({
-                    top: target.offsetTop - 50,
-                    behavior: "smooth"
-                });
-            }
-        });
-    });
+document.addEventListener("click", function (event) {
+  if (event.target.hasAttribute("data-tema")) {
+    const temaArquivo = event.target.getAttribute("data-tema");
+    const botoesTema = document.querySelectorAll("[data-tema]");
+    botoesTema.forEach((btn) => btn.classList.remove("tema-ativo"));
+    event.target.classList.add("tema-ativo");
+    document.getElementById("temaEstilo").setAttribute("href", temaArquivo);
+  }
 });
