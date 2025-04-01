4fd2d4a68721a0ba2afb506ace862b3f82aa1180
PPedrinho <peulima2004@gmail.com>
2025-04-01 19:31:13 -0300

    commit

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", function (event) {
-  if (event.target.hasAttribute("data-tema")) {
-    const temaArquivo = event.target.getAttribute("data-tema");
-    const botoesTema = document.querySelectorAll("[data-tema]");
-    botoesTema.forEach((btn) => btn.classList.remove("tema-ativo"));
-    event.target.classList.add("tema-ativo");
-    document.getElementById("temaEstilo").setAttribute("href", temaArquivo);
-  }
-});
